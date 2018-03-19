487d7bf2d89c139f8612602c28bee64d55624256
akim12 <akim.mezouari@gmail.com>
2018-03-19 11:31:04 +0100

    ajout questions du qcm

---
+++ qcm.php
@@ -0,0 +1,27 @@
+<?php
+echo htmlentities 
+securise les failles xss
+comme html specialchar
+ctype_digit verifie si 
+
+in_array()
+array_key_exists(key, array)
+array_search(needle, haystack);
+
+type natif 
+**********
+indefie null, bol, number
+
+aggregatation
+*************
+count(), sum(), average(), max(), min()
+
+ajax en javascript
+XMLHttpRequest
+
+
+?>
+
+
+
+
